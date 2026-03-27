428c5bb3d0ab56e909e2d347c772ce7f0b00844c
yenyic <49718423+yenyic@users.noreply.github.com>
2026-03-27 16:59:38 -0400

    Made a new scene cursor(miss spelled as crusor) and added a image for temporary use

---
+++ techgamejam2026/scenes/base UI/sprite_2d.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	z_index = 100
+	
+func _process(delta: float) -> void:
+	global_position = lerp(global_position, get_global_mouse_position(), 16.5*delta)
+	
+	var desired_rotation: float = -12.5 if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) else 0.0
+	rotation_degrees = lerp(rotation_degrees, desired_rotation, 16.5*delta)
+	
+	var desired_scale: Vector2 = Vector2(0.35, 0.35) if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) else Vector2(0.4, 0.4)
+	scale = lerp(scale, desired_scale, 16.5*delta)
